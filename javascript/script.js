22ba52d59d933b08fa2bed13f87ed692fcd0ef68
reza shokri <rezashokri.js@gmail,com>
2022-01-19 12:13:47 +0330

    chaneg api server

--- javascript/script.js
+++ javascript/script.js
@@ -20,7 +20,7 @@ let filtered = {
 // listener after load page
 document.addEventListener("DOMContentLoaded", () => {
   axios
-    .get("http://localhost:3000/items")
+    .get("https://fakestoreapi.com/products")
     .then((response) => {
       allProducts = response.data;
       renderProducts(allProducts, filtered);
@@ -39,23 +39,24 @@ filter.addEventListener("click", (e) => {
       renderProducts(allProducts, filtered);
       break;
 
-    case "watch":
-      filtered.sortItems = "watch";
+    case "men":
+      filtered.sortItems = "men's clothing";
+
       renderProducts(allProducts, filtered);
       break;
 
-    case "shirt":
-      filtered.sortItems = "shirt";
+    case "electronics":
+      filtered.sortItems = "electronics";
       renderProducts(allProducts, filtered);
       break;
 
-    case "gown":
-      filtered.sortItems = "gown";
+    case "women's clothing":
+      filtered.sortItems = "women's clothing";
       renderProducts(allProducts, filtered);
       break;
 
-    case "jewelry":
-      filtered.sortItems = "jewelry";
+    case "jewelery":
+      filtered.sortItems = "jewelery";
       renderProducts(allProducts, filtered);
       break;
     default:
@@ -68,19 +69,22 @@ filter.addEventListener("click", (e) => {
 // render page for add to ui
 function renderProducts(_allProducts, _filtered) {
   let filteredProducts = _allProducts.filter((product) => {
-    return product.title
-      .toLowerCase()
-      .includes(_filtered.sortItems.toLowerCase());
+    if (_filtered.sortItems == "") {
+      return product;
+    }
+    return product.category.toLowerCase() == _filtered.sortItems.toLowerCase();
   });
   products.innerHTML = "";
   filteredProducts.forEach((product) => {
+    let title = product.title.split(" ").splice(0, 2).join(" ");
+
     let newProduct = document.createElement("div");
     newProduct.classList = "product";
     newProduct.innerHTML = `
             <div class="img-product"><img src=${product.image} alt=""></div>
             <div class="product-information">
                 <div class="price-product">$ <span>${product.price}</span></div>
-                <div class="title-product">${product.title}</div>
+                <div class="title-product">${title}</div>
             </div>
             `;
     products.appendChild(newProduct);
